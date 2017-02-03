d91a32fd66368c79e594dbf1bcf2a6c68a2bd7d8
Nick Kibish <nickkibish@MacBook-Pro-Nick.local>
2017-02-03 19:49:26 +0200

    removed setters from Parameter Protocol

--- CascadePresenter/Protocols/AnimationParameters.swift
+++ CascadePresenter/Protocols/AnimationParameters.swift
@@ -11,24 +11,22 @@ import Foundation
 /// Protocol provides parametes of prestetion or dismission animation
 public protocol CascadeAnimationParameters {
     /// Duration of animation.
-    var animationDuration: TimeInterval { get set }
+    var animationDuration: TimeInterval { get }
     /// Alpha of presenting view controller.
-    var presentingVCAlpha: CGFloat { get set }
+    var presentingVCAlpha: CGFloat { get }
     /// Lenth from top of presenting view controller to the top of the screen.
-    var presentingTopMargin: CGFloat { get set  }
+    var presentingTopMargin: CGFloat { get  }
     /// Lenth from top of presented view controller to the top of the screen.
-    var presentedTopMargin: CGFloat { get set }
+    var presentedTopMargin: CGFloat { get }
     /// Scale of the presenting view controller.
-    var scale: CGFloat { get set }
+    var scale: CGFloat { get }
     /// Background color.
-    var backgroundColor: UIColor { get set }
+    var backgroundColor: UIColor { get }
     /// Corner radius of the viewControllers.
-    var cornerRadius: CGFloat { get set }
-    
-    var viewTag: Int { get }
+    var cornerRadius: CGFloat { get }
 }
 
-extension CascadeAnimationParameters {
+internal extension CascadeAnimationParameters {
     var viewTag: Int {
         return 101
     }
