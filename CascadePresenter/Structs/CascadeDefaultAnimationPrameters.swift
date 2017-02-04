bfa8bcd54e1f174c61769cdf3aa2e3a481fb9785
Nick Kibish <nickkibish@MacBook-Pro-Nick.local>
2017-02-04 19:39:48 +0200

    added parameters like in standard Music App

--- CascadePresenter/Structs/CascadeDefaultAnimationPrameters.swift
+++ CascadePresenter/Structs/CascadeDefaultAnimationPrameters.swift
@@ -14,14 +14,14 @@ struct CascadeDefaultAnimationPrameters: CascadeAnimationParameters {
     var animationDuration: TimeInterval = 0.5
     /// = 0.7
     var presentingVCAlpha: CGFloat = 0.7
-    /// = 20.0
-    var presentingTopMargin: CGFloat = 20
-    /// = 40.0
-    var presentedTopMargin: CGFloat  = 40
-    /// = 0.9
-    var scale: CGFloat = 0.9
+    /// = 20.5
+    var presentingTopMargin: CGFloat = 20.5
+    /// = 28.0
+    var presentedTopMargin: CGFloat  = 28
+    /// = 0.96
+    var scale: CGFloat = 0.96
     /// = .black
     var backgroundColor: UIColor = UIColor.black
-    /// = 0
-    var cornerRadius: CGFloat = 0
+    /// = 5
+    var cornerRadius: CGFloat = 5
 }
