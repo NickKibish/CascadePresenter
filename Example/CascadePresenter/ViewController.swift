2a933389840f92760ebb3998931b39f586e2a756
Nick Kibish <nickkibish@MacBook-Pro-Nick.local>
2017-02-05 01:51:39 +0200

    added test parameters to example project

--- Example/CascadePresenter/ViewController.swift
+++ Example/CascadePresenter/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 import CascadePresenter
 
+struct Params: CascadeAnimationParameters {
+    var animationDuration: TimeInterval = 1.5
+    var presentingVCAlpha: CGFloat = 0.97
+    var presentingTopMargin: CGFloat = 25
+    var presentedTopMargin: CGFloat  = 50
+    var scale: CGFloat = 1
+    var backgroundColor: UIColor = .green
+    var cornerRadius: CGFloat = 15
+}
+
 class ViewController: UIViewController {
+    let params = Params()
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         segue.destination.transitioningDelegate = self
     }
@@ -17,10 +29,10 @@ class ViewController: UIViewController {
 
 extension ViewController: UIViewControllerTransitioningDelegate {
     func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return Cascade.animator(for: .present)
+        return Cascade.animator(for: .present, animationParams: params)
     }
     
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return Cascade.animator(for: .dismiss)
+        return Cascade.animator(for: .dismiss, animationParams: params)
     }
 }
